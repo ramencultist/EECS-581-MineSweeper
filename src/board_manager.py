20913cbe49e7854d47ef028fcc1e50ec48ad5819
DrewMedlock1 <drewmedlock1@gmail.com>
2026-09-09 12:18:53 -0500

    Create prologue comment format.

---
+++ src/board_manager.py
@@ -0,0 +1,10 @@
+"""
+Module Name: board_manager.py
+Description: Hold and manage a 2D list of cells
+Inputs:
+Outputs:
+External Sources:
+Attributions:
+Authors: Drew Medlock
+Creation Date: 9/9/2026
+"""
