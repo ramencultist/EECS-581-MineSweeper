704710884e6a707977a0ff10c141f5684bc1eaec
DrewMedlock1 <drewmedlock1@gmail.com>
2026-09-11 14:26:08 -0500

    Add a cell class.
    This class will be held by a board manager as the states of each cell.

--- src/board_manager.py
+++ src/board_manager.py
@@ -8,3 +8,34 @@ Attributions:
 Authors: Drew Medlock
 Creation Date: 9/9/2026
 """
+
+class Cell:
+    def __init__(self):
+        self._covered = True
+        self._flagged = False
+        self._mine = False
+
+    # Getters
+
+    def isMine(self):
+        return self._mine
+
+    def isCovered(self):
+        return self._covered
+
+    def isFlagged(self):
+        return self._flagged
+
+    # Setters
+
+    def uncover(self):
+        self._covered = False
+
+    def setMine(self):
+        self._mine = True
+
+    def setFlagged(self):
+        self._flagged = True
+
+    def unFlag(self):
+        self._flagged = False
