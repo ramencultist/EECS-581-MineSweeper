1a79a1d85df8e68cd0fe4491b83d88d2427c6485
DrewMedlock1 <drewmedlock1@gmail.com>
2026-09-14 16:47:56 -0500

    Added type hints for return values

--- src/board_manager.py
+++ src/board_manager.py
@@ -23,20 +23,20 @@ class Cell:
     Cell class holds the states of each individual cell, and has functions for getting and setting these states.
     Code is original written by Drew Medlock
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self._covered = True
         self._flagged = False
         self._is_mine = False
 
     # Getters
 
-    def is_mine(self):
+    def is_mine(self) -> bool:
         return self._is_mine
 
-    def is_covered(self):
+    def is_covered(self) -> bool:
         return self._covered
 
-    def is_flagged(self):
+    def is_flagged(self) -> bool:
         return self._flagged
 
     # Setters
@@ -44,15 +44,15 @@ class Cell:
     def uncover(self):
         self._covered = False
 
-    def set_mine(self):
+    def set_mine(self) -> None:
         self._is_mine = True
 
-    def set_flagged(self):
+    def set_flagged(self) -> None:
         self._flagged = True
 
-    def remove_flag(self):
+    def remove_flag(self) -> None:
         self._flagged = False
 
-class BoardManager:
-    def __init__(self):
+class Board:
+    def __init__(self) -> None:
         self.board = [[Cell() for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
