ae78c5e654b4128b233c1a0e303ebbbf30828b39
DrewMedlock1 <drewmedlock1@gmail.com>
2026-09-14 21:12:32 -0500

    Update documentation.

--- src/board_manager.py
+++ src/board_manager.py
@@ -6,7 +6,7 @@ Class Name: Cell
 Description: Hold the states for each cell inside of the board
 
 Class Name: BoardManager
-Description: Hold the 10x10 grid of cells as a 2D list.
+Description: Hold the 10x10 grid of cells as a 2D list. Contain methods for updating the cell states.
 
 Inputs:
 Outputs:
@@ -29,30 +29,38 @@ class Cell:
         self.is_mine = False
 
 class BoardManager:
-    def __init__(self) -> None:
+    """
+    Board Manager class initializes to a fresh board of cells of nxn where n is the board size
+    Contains methods for reading from and updating the cells of the board.
+    """
+    def __init__(self) -> None: # Original code written by Drew Medlock
         self.board = [[Cell() for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
-    def is_mine(self, row: int, col: int) -> bool:
+    # Getters
+
+    def is_mine(self, row: int, col: int) -> bool: # Original code written by Drew Medlock
         return self.board[row][col].is_mine
 
-    def is_covered(self, row: int, col: int) -> bool:
+    def is_covered(self, row: int, col: int) -> bool: # Original code written by Drew Medlock
         return self.board[row][col].covered
 
-    def is_flagged(self, row: int, col: int) -> bool:
+    def is_flagged(self, row: int, col: int) -> bool: # Original code written by Drew Medlock
         return self.board[row][col].flagged
 
-    def uncover_cell(self, row: int, col: int) -> None:
+    # Setters
+
+    def uncover_cell(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board[row][col].covered = False
 
-    def set_mine(self, row: int, col: int) -> None:
+    def set_mine(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board[row][col].is_mine = True
 
-    def set_flagged(self, row: int, col: int) -> None:
+    def set_flagged(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board[row][col].flagged = True
 
-    def remove_flag(self, row: int, col: int) -> None:
+    def remove_flag(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board[row][col].flagged = False
 
-    def set_mines(self, mines: list[tuple]) -> None:
+    def set_mines(self, mines: list[tuple]) -> None: # Original code written by Drew Medlock
         for mine_location in mines:
             self.set_mine(row=mine_location[0], col=mine_location[1])
--- src/game_logic.py
+++ src/game_logic.py
@@ -2,6 +2,9 @@
 Module Name: game_logic.py
 Description: Handle game logic and make calls to board_manager objects
 
+Class Name: GameManager
+Descrption: Handle all of the hidden game state knowledge and set the states of the board, following the game logic
+
 Inputs:
 Outputs:
 External Sources:
@@ -14,7 +17,18 @@ import board_manager
 import random # Random is used to generate the mine locations
 
 class GameManager:
-    def __init__(self, num_mines) -> None:
+    """
+    Game manager is responsiglve for interacting with the board to update it accordinging to the game logic.
+    """
+    def __init__(self, num_mines) -> None: # Original code written by Drew Medlock
+        """
+        Has states for the running of the game.
+        Gets a board object
+        is_lost and is_won are set to easily identify if the game has ended
+        num_mines is the number of mines given from the user selection
+        mine_count is the number of mines - the number of flags
+        Cells to clear is the amount of cells that haven't been cleared/uncovered of the safe cells
+        """
         self.board = board_manager.BoardManager()
         self.is_lost = False
         self.is_won = False
@@ -24,12 +38,15 @@ class GameManager:
         self.cells_to_clear = board_manager.BOARD_SIZE ** 2 - self.num_mines
         self.populate_mines()
 
-    def populate_mines(self) -> None:
+    def populate_mines(self) -> None: # Original code written by Drew Medlock
+        """
+        Generates a list of all the possible locations, then picks the mines from it
+        """
         locations = [(row, col) for row in range(board_manager.BOARD_SIZE) for col in range(board_manager.BOARD_SIZE)]
         mine_locations = random.sample(locations, self.num_mines)
         self.board.set_mines(mine_locations)
 
-    def reveal_cell(self, row: int, col: int) -> None:
+    def reveal_cell(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         # Being flagged prevents the cell from being uncovered
         if not self.board.is_flagged(row, col):
             self.board.uncover_cell(row, col)
@@ -38,14 +55,14 @@ class GameManager:
             else:
                 self.cells_to_clear -= 1
 
-    def flag_cell(self, row: int, col: int) -> None:
+    def flag_cell(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board.set_flagged(row, col)
         self.flags += 1
         # Check for preventing mine_count to going to negative if more than num_mines flags are placed
         if self.mine_count > 0:
             self.mine_count -= 1
 
-    def unflag_cell(self, row: int, col: int) -> None:
+    def unflag_cell(self, row: int, col: int) -> None: # Original code written by Drew Medlock
         self.board.remove_flag(row, col)
         self.flags -= 1
         # If there are more flags than the number of mines, then the mine_count shouldn't be increased from 0
